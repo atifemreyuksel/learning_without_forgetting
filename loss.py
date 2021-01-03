86eed207614f79c01e3f74603fea0d71c4653d81
atifemreyuksel <atifemreyuksel@gmail.com>
2021-01-03 20:45:43 +0300

    Implement KDLoss

--- loss.py
+++ loss.py
@@ -1,23 +1,29 @@
 from torch import nn
+from torch.nn import functional as F
 
 class KDLoss():
     def __init__(self, temp=2):
         self.temp = temp
     
-    def __call__(self, preds, old_gts):
-        pass
+    def __call__(self, preds, gts):
+        l_preds = F.log_softmax(preds, dim=-1)
+        l_gts = F.log_softmax(gts, dim=-1)
+        loss = F.nll_loss(l_preds, l_gts)
+        return loss
 
 class TotalLoss():
-    def __init__(self, strategy="lwf", lambda_old=1., temp=2):
+    def __init__(self, strategy="lwf", num_new_classes=10, lambda_old=1., temp=2):
         self.kd_loss = KDLoss(temp=temp)
         self.ce_loss = nn.CrossEntropyLoss() 
         self.strategy = strategy
+        self.num_new_classes = num_new_classes
         self.lambda_old = lambda_old
 
-    def __call__(self, preds, gts, old_gts):
+    def __call__(self, preds, gts, old_preds=None, old_gts=None):
         if self.strategy == "lwf":
-            preds_old, preds_new = preds[:, :-10], preds[:, -10:]
-            old_task_loss = self.kd_loss(preds_old, old_gts)
+            #preds_old, preds_new = preds[:, :-self.num_new_classes], preds[:, -self.num_new_classes:]
+            preds_new = preds[:, -self.num_new_classes:]
+            old_task_loss = self.kd_loss(old_preds, old_gts)
             new_task_loss = self.ce_loss(preds_new, gts)
             total_loss = self.lambda_old * old_task_loss + new_task_loss
         else:
