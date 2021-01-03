bf12a01be93a2579eafe8289b4603c7c6ff52e20
atifemreyuksel <atifemreyuksel@gmail.com>
2021-01-03 21:49:11 +0300

    Fix loss logic for old task

--- loss.py
+++ loss.py
@@ -20,12 +20,11 @@ class TotalLoss():
         self.lambda_old = lambda_old
 
     def __call__(self, preds, gts, old_preds=None, old_gts=None):
+        preds_old, preds_new = preds[:, :-self.num_new_classes], preds[:, -self.num_new_classes:]
         if self.strategy == "lwf":
-            #preds_old, preds_new = preds[:, :-self.num_new_classes], preds[:, -self.num_new_classes:]
-            preds_new = preds[:, -self.num_new_classes:]
-            old_task_loss = self.kd_loss(old_preds, old_gts)
-            new_task_loss = self.ce_loss(preds_new, gts)
-            total_loss = self.lambda_old * old_task_loss + new_task_loss
+            old_task_loss = self.kd_loss(preds_old, old_gts)
         else:
-            total_loss = self.ce_loss(preds, gts)
+            old_task_loss = 0.
+        new_task_loss = self.ce_loss(preds_new, gts)
+        total_loss = self.lambda_old * old_task_loss + new_task_loss
         return total_loss
